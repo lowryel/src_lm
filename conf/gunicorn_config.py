58febbb0c0c929e4f904e1812ce035b5c318eb96
Eugene Agbaglo <eugeneagbaglo@gmail.com>
2023-09-20 12:26:10 +0000

    Update gunicorn_config.py

--- conf/gunicorn_config.py
+++ conf/gunicorn_config.py
@@ -1,6 +1,8 @@
-command = '~/.virtual-env/bin/gunicorn'
-pythonpath = '/home/lowry/Backend/src_lm/lm'
-bind = '127.0.0.1'
+# command = '~/.virtual-env/bin/gunicorn'
+# pythonpath = '/home/lowry/Backend/src_lm/lm'
+# bind = '127.0.0.1'
+# workers = 3
+
+pythonpath = '/src_lm/lm'
+bind = 'https://lm-estate.onrender.com/'
 workers = 3
-
-
