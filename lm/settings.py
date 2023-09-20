a7842ef4fc7d2c8b6780229385f5a54f89c006eb
lowryel <eugeneagbaglo@gmail.com>
2023-09-20 14:21:40 +0000

    a

--- lm/settings.py
+++ lm/settings.py
@@ -21,7 +21,7 @@ environ.Env.read_env(BASE_DIR / '.env')  # <-- Updated!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
